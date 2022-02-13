033ad761633bff8d0224069b1e79885eb7c7fd87
ericatsu <ekaatsu@st.ug.edu.gh>
2022-02-13 18:04:40 +0000

    font color not changing

--- src/elements/MemberElements.jsx
+++ src/elements/MemberElements.jsx
@@ -2,15 +2,32 @@ import styled from "styled-components";
 
 export const MemberContainer = styled.div`
     margin-top: 60px;
-    background-color: white;
-    box-sizing: border-box;
+    background-color: rgb(255, 255, 255);
     text-align: center;
+    display: block;
+    padding: 40px 0;
+    margin: 15px;
+
+    &:after, :before{
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+`
+
+export const MemberSub = styled.div`
     padding-left: 15px;
     padding-right: 15px;
     margin-right: auto;
     margin-left: auto;
-    display: block;
-    line-height: 60px;
+    
+
+    @media (min-width: 992px){
+    width: 970px;
+    }
+
+    @media (min-width: 768px){
+    width: 750px;
+    }
 `
 
 export const Image = styled.img`
@@ -38,27 +55,154 @@ export const Slogan = styled.h1`
 `
 
 export const MemberRow = styled.div`
-    margin-left: -15px;
-    margin-right: -15px;
-    text-align: left;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+  text-align: left;
 `
 
-export const MemberColumn = styled.div`
-    min-width: 768px;
+export const MemberColumnRight = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
     padding-right: 50px;
+    float: left;
+    padding-right: 50px;
+`
+
+export const MemberColumnLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-basis: 100%;
+    flex: 1;
+    padding-right: 50px;
+    background-color: blue;
+    float: left;
+    padding-left: 50px;
+
+    @media screen and (min-width: 768px){
     width: 50%;
     float: left;
+    position: relative;
+    min-height: 1px;
+    padding-right: 15px;
+    }
 `
 
 export const MemberH2 = styled.h2`
     text-transform: none;
     font-size: 26px;
     margin: 15px 0 10px 0;
-    line-height: 2;
+    color: ##75adee;
 `
 export const MemberP = styled.p`
     padding: 0;
-    margin-top: 2em;
-    margin-bottom: 2em;
-    line-height: 2;
+    margin-top: 1em;
+    margin-bottom: 1em;
+    line-height: 1.5;
+    margin: 3px 0 10px 0;
+    font-size: 16px;
+    color: ##03274d;
+`
+
+export const FormContainer = styled.div`
+  margin-bottom: 20px;
+  `
+
+export const Request = styled.h2`
+  margin: 15px 0 10px 0;
+  text-align: center;
+  max-width: 100%;
+  height: auto;
+  font-size: 26px;
+`;
+
+export const InputContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: auto;
+  width: 100%;
+  max-width: 100%;
+  position: relative;
+`
+export const Input = styled.input`
+    border-color: #cbd5df;
+    color: #7e96ae;
+    border: #fff 1px solid;
+    outline: none!important;
+    background: transparent;
+    width: 35vw;
+    height: 35px;
+    margin: 10px auto;
+    border-radius: 3px;
+    padding-left: 5px;
+`
+export const TextArea = styled.textarea`
+    resize: none;
+    height: 90px;
+    padding-top: 10px;
+    width: 35vw;
+    margin: 10px auto;
+    border-color: #cbd5df;
+    color: #7e96ae;
+    border: #fff 1px solid;
+    outline: none!important;
+    background: transparent;
+`
+
+export const ButtonContainer = styled.div`
+  margin: 1rem 0 2rem 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.button`
+  height: 75px;
+  width: 35vw;
+  display: inline-block;
+  border-radius: 20px;
+  color: #fff;
+  text-align: center;
+  font-weight: 300;
+  font-size: 30px;
+  padding: 5px;
+  margin: 10px auto;;
+  cursor: pointer;
+  border: none;
+  -webkit-appearance: none;
+   background: linear-gradient(to bottom,#419efe 50%,#328fef 51%);
+`;
+
+export const CautionContainer = styled.div`
+    padding: 0px 0 5px 0;
+    font-style: italic;
+    font-size: 15px;
+    line-height: 17px;
+    margin-left: 15px;
+    
+
+    @media screen and (max-width: 991px) {
+    font-size: 14px;
+    line-height: 16px;
+    margin-left: 15px;
+    }
+`
+
+export const Caution = styled.img`
+    float: left;
+    margin: 6px 8px 0 0;
+    display: inline-block;
+    position: relative;
+    width: 22px;
+    height: 22px;
+
+    @media screen and (max-width: 991px){
+        margin-top: 4px;
+    }
 `
--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -1,37 +1,52 @@
 import React from 'react';
-import { MemberContainer, Image, MemberH1, Slogan, MemberRow, MemberColumn, MemberH2, MemberP } from '../elements/MemberElements'
+import { MemberContainer, Image, MemberH1, Slogan, MemberRow,  MemberH2, MemberP, MemberSub, MemberColumnRight, MemberColumnLeft, FormContainer, Request, InputContainer, ButtonContainer, Input, TextArea, Button, CautionContainer, Caution } from '../elements/MemberElements';
+import mark from "../assets/images/mark.png"
 
 function Member() {
   return (
    <MemberContainer>
+     <MemberSub>
         <Image src="" alt="" className="image" />
         <MemberH1>CleanIt Service Limited</MemberH1>
         <Slogan>
             CUSTOMER SATISFACTION IS THE FOUNDATION OF ANY SUCCESSFUL BUSINESS
         </Slogan>
+      </MemberSub>
         <MemberRow>
-            <MemberColumn>
-             <MemberH2>ABOUT US</MemberH2>
+            <MemberColumnRight>
+              <Request>Why Become A Member</Request>
+             <MemberH2>Reduce Prices</MemberH2>
              <MemberP>CleanIt Service is a guarented to become the one best cleaning industry in country. We priotize to meet our clients highest standards. Due to our dedicated team of trained specialists, we value each and every client. We never give up on any stain and we train only the best personnel.</MemberP>
              {/* <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP> */}
-             <MemberH2>WHAT WE OFFER</MemberH2>
+             <MemberH2>Save Time</MemberH2>
              <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
              <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-             <MemberH2>WHAT WE OFFER</MemberH2>
+             <MemberH2>Re-Cleaning Guarantee</MemberH2>
              <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
              <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-            </MemberColumn>
-            <div className="col-sm-6">
-             <MemberH2>ABOUT US</MemberH2>
-             <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-             <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-             <MemberH2>WHAT WE OFFER</MemberH2>
-             <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-             <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-             <MemberH2>WHAT WE OFFER</MemberH2>
-             <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-             <MemberP>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit minima molestias autem, illo quo labore.</MemberP>
-            </div>
+            </MemberColumnRight>
+            <MemberColumnLeft>
+             <FormContainer>
+               <Request>Be A Member Today</Request>
+               <InputContainer>
+               <Input type="text" placeholder="Full Name" />
+               <Input type="text" placeholder="Email" />
+               <Input type="text" placeholder="Phone" />
+               <Input type="text" placeholder="Location" />
+               <TextArea type="text" placeholder="Comment / Service" spellcheck="false"/>
+               <ButtonContainer>
+               <Button placeholder="Become A Member" >Become A Member </Button>
+               </ButtonContainer>
+               <CautionContainer>
+                <Caution src={mark}/>  
+                {/* Increase image size */}
+                Your information is never sold or shared.
+                <br/>
+              It is used for the sole purpose of contacting you.
+             </CautionContainer>
+               </InputContainer>
+            </FormContainer>
+            </MemberColumnLeft>
             <div className="textcolumn"></div>
         </MemberRow>
       </MemberContainer>
