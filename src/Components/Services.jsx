f44422e34ab6b7b18e3544343f202aa63079ec2b
ericatsu <ekaatsu@st.ug.edu.gh>
2022-06-09 00:04:42 +0000

    creating services

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,48 +1,110 @@
-import React from 'react'
-import { ServicesContainer, ServicesH1, ServicesWrapper,
-        ServicesCard, ServicesIcon, ServicesH2,
-    ServicesP } from '../elements/ServicesElement'
-import webdev from '../assets/images/webdev.svg'
-import appdev from '../assets/images/appdev.svg'
-import other from '../assets/images/other.svg'
+import React, { useState, useEffect} from 'react'
+import { ServicesContainer, ServicesH1, ServicesWrapper,ServicesCard, ServicesIcon, ServicesH2,ServicesP, ServicesPortfolio } from '../elements/ServicesElement'
+import about01 from '../assets/images/about01.png';
+import about02 from '../assets/images/about02.png';
+import about03 from '../assets/images/about03.png';
+import about04 from '../assets/images/about04.png';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+const services = [
+  { title: 'Mission & Vission', description: 'The Company mission', imgUrl: about01},
+  { title: 'Mission & Vission', description: 'The Company mission', imgUrl: about02},
+  { title: 'Mission & Vission', description: 'The Company mission', imgUrl: about03},
+  { title: 'Mission & Vission', description: 'The Company mission', imgUrl: about04}
+]
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(services);
+      } else {
+        setFilterWork(services.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+
     return (
+        <>
         <ServicesContainer id="Services">
             <ServicesH1>Our Services</ServicesH1>
-            <ServicesWrapper>
-                <ServicesCard>
-                    <ServicesIcon src={webdev}/>
-                    <ServicesH2>House Cleaning</ServicesH2>
-                    <ServicesP>Develop a website for your businesses and organizations.</ServicesP>
-                </ServicesCard>
-                <ServicesCard>
-                    <ServicesIcon src={appdev} />
-                    <ServicesH2>Public Cleansing</ServicesH2>
-                    <ServicesP>We specialize in mobile app design and development.</ServicesP>
-                </ServicesCard>
-                <ServicesCard>
-                    <ServicesIcon src={other} />
-                    <ServicesH2>Others</ServicesH2>
-                    <ServicesP>We offer other services as well. Lorem ipsum dolor ?</ServicesP>
-                </ServicesCard>
-                <ServicesCard>
-                    <ServicesIcon src={webdev}/>
-                    <ServicesH2>House Cleaning</ServicesH2>
-                    <ServicesP>Develop a website for your businesses and organizations.</ServicesP>
-                </ServicesCard>
-                {/* <ServicesCard>
-                    <ServicesIcon src={appdev} />
-                    <ServicesH2>Public Cleansing</ServicesH2>
-                    <ServicesP>We specialize in mobile app design and development to create.</ServicesP>
-                </ServicesCard>
-                <ServicesCard>
-                    <ServicesIcon src={other} />
-                    <ServicesH2>Others</ServicesH2>
-                    <ServicesP>We offer other services as well. Lorem ipsum dolort huet jsit?</ServicesP>
-                </ServicesCard> */}
-            </ServicesWrapper>
+            <ServicesFilter>
+                {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <ServiceFilterItem
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </ServiceFilterItem>
+           ))}
+        </ServicesFilter>
+            
+                <ServicesPortfolio
+                  animate={animateCard}
+                  transition={{ duration: 0.5, delayChildren: 0.5 }}
+                >
+            {filterWork.map((service, index) => (
+            <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={service.imgUrl} alt={service.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={service.projectLink} target="_blank" rel="noreferrer">
+
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={service.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{service.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{service.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{service.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </ServicesPortfolio>
         </ServicesContainer>
+    </>
     )
 }
 
--- src/elements/ServicesElement.jsx
+++ src/elements/ServicesElement.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const ServicesContainer = styled.div`
  height: 450px;
@@ -19,48 +20,15 @@ export const ServicesContainer = styled.div`
  }
 `
 
-export const ServicesWrapper = styled.div`
-   max-width: 1500px;
-  /*margin: 0 auto; */
-  display: grid;
-  /* grid-template-columns: 1fr 1fr 1fr 1fr;
-  align-items: center;
-  grid-gap: 20px; */
-  padding: 0 50px; 
-  width: 100%;
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-  grid-gap: 20px;
-
-  /* @media screen and (max-width: 1000px){
-    grid-template-columns: 1fr 1fr;
- }
-
- @media screen and (max-width: 768px){
-     grid-template-columns: 1fr;
-     padding: 0 20px;
- } */
+export const ServicesFilter = styled.div`
+   
 `
 
-export const ServicesCard = styled.div`
- background: #fff;
- display: flex;
- flex-direction: column;
- justify-content: flex-start;
- align-items: center;
- border-radius: 10px;
- max-height: 340px;
- padding: 30px;
- transition: all 0.2s ease-in-out;
- box-shadow: 0 1px 3px rgb(0,0,0,0.2);
-
- &:hover{
-     transform: scale(1.02);
-     transition: all 0.2s ease-in-out;
-     cursor: pointer;
- }
+export const ServicesFilterItem = styled.div`
+ 
 `
 
-export const ServicesIcon = styled.img`
+export const ServicesPortfolio = styled(motion.div)`
   height: 160px;
   width: 160px;
   margin-bottom: 10px;
